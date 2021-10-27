a4be2f7d24541dde819041b22e5fc3bdeeb5f908
Ocup <ocup0311@gmail.com>
2021-10-27 17:31:02 +0800

    ADD "Simplify" utility Type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,14 @@
 interface Todo {
-  text: string;
-  complete: boolean;
-  }
+  text: string
+  complete: boolean
+}
 
-type ToggleTodo = (selectedTodo: Todo) => void;
+type ToggleTodo = (selectedTodo: Todo) => void
 
-type AddTodo = (text: string) => void;
+type AddTodo = (text: string) => void
+
+type Simplify<T> = T extends Record<string, any>
+  ? { [K in keyof T]: Simplify<T[K]> }
+  : T
+
+type SimplifyTodo = Simplify<Todo>
